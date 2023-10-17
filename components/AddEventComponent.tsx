65eb1dbdc14738bf0645ff113c029c54c44817df
FestersNephew <unclefestersnephew@gmail.com>
2023-10-17 13:54:25 -0400

    working on android, still need to fix ios datepicker

--- components/AddEventComponent.tsx
+++ components/AddEventComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Pressable } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet, Pressable, Platform, TouchableOpacity } from 'react-native';
 import addEventToFirestore from './AddEventToFirestore';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
@@ -10,6 +10,7 @@ interface AddEventComponentProps {
 const AddEventComponent: React.FC<AddEventComponentProps> = ({ onClose }) => {
   const [eventName, setEventName] = useState('');
   const [eventDescription, setEventDescription] = useState('');
+  const [eventDate, setEventDate] = useState('' as string | undefined);
   const [date, setDate] = useState(new Date());
   const [eventLocation, setEventLocation] = useState('');
   const [showPicker, setShowPicker] = useState(false);
@@ -43,24 +44,36 @@ const AddEventComponent: React.FC<AddEventComponentProps> = ({ onClose }) => {
       const currentDate = selectedDate || new Date();
       setShowPicker(false);
       setDate(currentDate);
+
+      if (Platform.OS === 'android') {
+        toggleDatePicker();
+        setEventDate(currentDate.toDateString());
+      }
+
     } else {
       setShowPicker(false);
     }
   };
   
-
+  const confirmIOSDate = () => {
+    setEventDate(date.toDateString());
+    toggleDatePicker();
+  };
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Add Event</Text>
       <TextInput
         style={styles.input}
         placeholder="Event Name"
+        placeholderTextColor="#201D28"
         value={eventName}
         onChangeText={setEventName}
       />
       <TextInput
         style={styles.input}
         placeholder="Event Description"
+        placeholderTextColor="#201D28"
         value={eventDescription}
         onChangeText={setEventDescription}
       />
@@ -73,21 +86,63 @@ const AddEventComponent: React.FC<AddEventComponentProps> = ({ onClose }) => {
           display="spinner"
           value={date}
           onChange={onChange}
+          style={styles.datePicker}
         />
       )}
 
-      <Pressable>
-      <TextInput
-        style={styles.input}
-        placeholder="Event Date"
-        onChangeText={Date}
-        editable={false}
-      />
-      </Pressable>
+      {showPicker && Platform.OS === 'ios' && (
+        <View
+          style={{ flexDirection: "row", 
+          justifyContent: "space-around"}}
+        >
+          <TouchableOpacity style={[
+            styles.button,
+            styles.pickerButton,
+              { backgroundColor: "red" }
+            ]}
+            onPress={toggleDatePicker}
+            >
+            <Text 
+              style={[
+                styles.buttonText, { color: "white" }
+              ]}
+            >Cancel</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={[
+            styles.button,
+            styles.pickerButton,
+            ]}
+            onPress={confirmIOSDate}
+            >
+            <Text 
+              style={[
+                styles.buttonText,
+              ]}
+            >Confirm</Text>
+          </TouchableOpacity>
+
+        </View>
+        )}
+
+      {!showPicker && (
+        <Pressable>
+          <TextInput
+            style={styles.input}
+            placeholder="Event Date"
+            value={eventDate}
+            placeholderTextColor="#201D28"
+            onChangeText={Date}
+            editable={false}
+            onPressIn={toggleDatePicker}
+          />
+        </Pressable>
+      )}
 
       <TextInput
         style={styles.input}
         placeholder="Event Location"
+        placeholderTextColor="#201D28"
         value={eventLocation}
         onChangeText={setEventLocation}
       />
@@ -113,9 +168,31 @@ const styles = StyleSheet.create({
   input: {
     width: '100%',
     padding: 12,
-    borderColor: 'gray',
+    borderColor: 'grey',
     borderWidth: 1,
     marginBottom: 16,
+  },
+  datePicker: {
+    height: 120,
+    marginTop: -10,
+    color: "#201D28",
+  },
+  button: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+    marginTop: 10,
+    marginBottom: 15,
+    backgroundColor: "#075985"
+  },
+  pickerButton: {
+    paddingHorizontal: 20,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "500",
+    color: "#fff",
   },
 });
 
