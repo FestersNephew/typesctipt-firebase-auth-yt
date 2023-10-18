2669738e3b62b75ce6a351bfc9dd35e69edda250
FestersNephew <unclefestersnephew@gmail.com>
2023-10-18 11:51:58 -0400

    added expo av video player to resources.

--- src/screens/Resources.tsx
+++ src/screens/Resources.tsx
@@ -1,12 +1,56 @@
-import React from "react";
-import { View, Text } from "react-native";
+import * as React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { Video, ResizeMode } from 'expo-av';
 
 const Resources = () => {
-  return (
-    <View>
-      <Text>Resources Screen</Text>
-    </View>
-  );
-};
+    const video = React.useRef(null);
+    const [status, setStatus] = React.useState({});
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Videos 4 You</Text>
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: 'https://stream.mux.com/sxWjpI71s801h1mjSgIqe46xr7RfffR1Kp01SxAB02e6Qo.m3u8',
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
+        />
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: 'https://stream.mux.com/k8Co8LqKBrkcg3VZnD8sq00sVzLm02TGmQCwgKvhD16WQ.m3u8',
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={status => setStatus(() => status)}
+        />
+      </View>
+    );
+  }
 
 export default Resources;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    video: {
+      width: 300,
+      height: 300,
+    },
+    headerText:{
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10,
+        fontWeight: "bold"
+    }
+}); 
